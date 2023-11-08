a27b20ac3e7da64527cd3844da2a10293ecb2704
fuzzytechsoup <fuzzytechsoup@gmail.com>
2023-11-08 21:22:19 +0800

    exercise files

---
+++ index_v2_then.js
@@ -0,0 +1,43 @@
+//callback hell resolve using then
+//npm init
+//npm i superagent
+
+const fs = require('fs');
+const superagent = require('superagent');
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find the file');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('could not write file');
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Random dog image saved to file');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//pending promise becomes a resolved promise once its come back
